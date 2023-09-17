d704e187f14a73d80917bdcf3819de6c3542ed4e
Omeir34 <kai.omeir.x2@gmail.com>
2023-09-17 18:22:33 +0500

    Updated UI

--- SampleTestApp/SampleTestApp/ViewController.swift
+++ SampleTestApp/SampleTestApp/ViewController.swift
@@ -23,23 +23,62 @@ class ViewController: UIViewController {
             fileStoreManager: DefaultFileStoreManager()
        )
     }()
-
+    
+    private lazy var fetchButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Fetch Video", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(fetchVideo), for: .touchUpInside)
+        button.layer.cornerRadius = 8
+        button.clipsToBounds = true
+        return button
+    }()
+    
+    private lazy var activityIndicator: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.color = .blue
+        return spinner
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let button = UIButton(frame: CGRect(x: 40, y: 40, width: 100, height: 100))
-        button.setTitle("Fetch Video", for: .normal)
-        view.addSubview(button)
-        button.backgroundColor = .yellow
-        button.setTitleColor(.blue, for: .normal)
-        button.addTarget(self, action: #selector(fetchVideo), for: .touchUpInside)
+        addFetchButton()
+        addActivityIndicator()
+    }
+    
+    fileprivate func addActivityIndicator() {
+        view.addSubview(activityIndicator)
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityIndicator.widthAnchor.constraint(equalToConstant: 50),
+            activityIndicator.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    fileprivate func addFetchButton() {
+        view.addSubview(fetchButton)
+        NSLayoutConstraint.activate([
+            fetchButton.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 16),
+            fetchButton.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -16),
+            fetchButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -40),
+            fetchButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
     }
     
     @objc func fetchVideo() {
-        videoCacheManager.getVideo(with: URL(string: "https://www.pexels.com/download/video/7565438/")!) { result in
+        fetchButton.isEnabled = false
+        activityIndicator.startAnimating()
+        videoCacheManager.getVideo(with: URL(string: "https://www.pexels.com/download/video/18122212/?fps=29.97&h=640&w=360")!) { result in
             switch result {
             case .success(let fileURL):
                 if let fileURL {
                     DispatchQueue.main.async {
+                        self.activityIndicator.stopAnimating()
+                        self.fetchButton.isEnabled = true
                         let playerVC = AVPlayerViewController()
                         playerVC.player = AVPlayer(url: fileURL)
                         self.present(playerVC, animated: true)
